fcd694a58d7175bc5c47c31165b396d39ea8d344
marin0suke <monk.marina@gmail.com>
2024-12-17 11:13:08 +1000

    Add placeShip method and its tests for valid board placement, and occupied cells.

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,3 +1,5 @@
+import Ship from "./ship.js";
+
 export default class Gameboard {
     constructor(size = 10) {
         this.size = size;
@@ -5,16 +7,49 @@ export default class Gameboard {
     }
 
     initializeBoard() {
-        const rows = "ABCDEFGHIJ".slice(0, this.size).split("");
-        console.log(rows);
-        const board = rows.map((row) => Array.from({ length: this.size }, (_, col) => ({
+        const rows = "ABCDEFGHIJ".slice(0, this.size).split(""); // alphabet for corresponding rows. slice to custom size if specified. split to arrays so Array.from works.
+        const board = rows.map((row) => Array.from({ length: this.size }, (_, col) => ({ // practiced using Array.from instead of nested loop to generate grid.
             coordinate: `${row}${col + 1}`,
-            occupied: false,
-            clicked: false
+            occupied: false, 
+            clicked: false 
             }))
         )
-
         return board;
+    }
+
+    placeShip(length, coordinate, orientation) {
+        const rowIndex = coordinate.charCodeAt(0) - 65; // ascii E to 4.
+        const colIndex = parseInt(coordinate.slice(1)) - 1; // "5" to 4.
+
+        if (orientation !== "horizontal" && orientation !== "vertical") {
+            throw new Error("Orientation must be 'horizontal' or 'vertical'");
+        }
+
+        if ( // check if ship placement will be out of bounds. index + length of ship can't be more than the size of grid.
+            (colIndex + length > this.size && orientation === "horizontal") || 
+            (rowIndex + length > this.size && orientation === "vertical") 
+        ) {
+            throw new Error("Ship placement is out of bounds");
+        }
+
+        // next, separate checking for overlap from actually placing the ship.
+        //placing each part of the ship in a loop might cause partially placed ships.
+
+        for (let i = 0; i < length; i++) { // checking each cell for length of ship we will place.
+            const targetRow = orientation === "horizontal" ? rowIndex : rowIndex + i;
+            const targetCol = orientation === "horizontal" ? colIndex + i : colIndex;
+
+            if (this.grid[targetRow][targetCol].occupied) {
+                throw new Error("Cell is already occupied"); // if we encounter an occupied cell, we 
+            }
+        }
+        
+        for (let i = 0; i < length; i++) {
+            const targetRow = orientation === "horizontal" ? rowIndex : rowIndex + i;
+            const targetCol = orientation === "horizontal" ? colIndex + i : colIndex;
+    
+            this.grid[targetRow][targetCol].occupied = true;
+        }
     }
 }
 
--- tests/gameBoard.test.js
+++ tests/gameBoard.test.js
@@ -20,6 +20,32 @@ test("initializeBoard generates board squares with correct initialized propertie
             expect(cell.clicked).toBe(false);
         })
     })
-    
 })
 
+test("placeShip occupies the board with specified Ship details", () => {
+    const board = new Gameboard;
+    board.placeShip(3, "E5", "horizontal");
+    expect(board.grid[4][4].occupied).toBe(true);   
+    expect(board.grid[4][5].occupied).toBe(true);   
+    expect(board.grid[4][6].occupied).toBe(true);   
+})
+
+test("placeShip occupies the board vertically", () => {
+    const board = new Gameboard;
+    board.placeShip(3, "E5", "vertical");
+    expect(board.grid[4][4].occupied).toBe(true);   
+    expect(board.grid[5][4].occupied).toBe(true);   
+    expect(board.grid[6][4].occupied).toBe(true);   
+})
+
+test("placeShip throws an error when trying to place out of bounds of the board", () => {
+    const board = new Gameboard;
+    expect(() => board.placeShip(2, "J10", "horizontal")).toThrow("Ship placement is out of bounds");
+    expect(() => board.placeShip(3, "I5", "vertical")).toThrow("Ship placement is out of bounds");
+})
+
+test("placeShip throws an error when trying to place ship on occupied coordinate", () => {
+    const board = new Gameboard;
+    board.placeShip(3, "E5", "vertical");
+    expect(() => board.placeShip(3, "E5", "vertical")).toThrow("Cell is already occupied");
+})
